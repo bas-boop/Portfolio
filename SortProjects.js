9c981edf9e274e0800695cfb1c5d43aa4c508b24
bas-boop <dereusbas461@gmail.com>
2023-06-04 19:02:12 +0200

    removed unused code

--- SortProjects.js
+++ SortProjects.js
@@ -1,3 +1,4 @@
+const projectViews = Array.from(document.getElementsByClassName("view"));
 var currentProjectId = 'filter1';
 
 function toggleProjectDiv(divId)
@@ -8,7 +9,6 @@ function toggleProjectDiv(divId)
     // Hide the currently visible project
     if (currentProjectId !== null) {
         var currentProjectDiv = document.getElementById(currentProjectId);
-        var currentChildContainerDiv = currentProjectDiv.querySelector(".project-container");
 
         currentProjectDiv.style.display = "none";
         currentChildContainerDiv.style.display = "none";
@@ -17,18 +17,12 @@ function toggleProjectDiv(divId)
     // Show the selected project
     if (projectDiv.style.display === "none") {
         projectDiv.style.display = "block";
-        childContainerDiv.style.display = "block";
         currentProjectId = divId;
     } else {
         projectDiv.style.display = "none";
-        childContainerDiv.style.display = "none";
         currentProjectId = null;
     }
 }
-
-const projectViews = Array.from(document.getElementsByClassName("view"));
-
-// const temp = document.createElement('div');
 
 function toggleView(id) {
     projectViews.forEach(view => {
